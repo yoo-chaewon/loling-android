e68d50191f114c87c665abc5b1966903728147c1
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2019-02-02 08:24:11 +0900

    Add MyPageActivity, MyPageSettingActivity - MyPageActivity 뷰페이저로 수정 - MyPageSettingActivity 끝 - MainActivity 친구리스트 추가

--- app/src/main/java/mashup/loling/MainActivity.kt
+++ app/src/main/java/mashup/loling/MainActivity.kt
@@ -2,13 +2,22 @@ package mashup.loling
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import mashup.loling.Fragment.FriendListFragment
 import mashup.loling.model.FriendItem
 
 class MainActivity : AppCompatActivity() {
+
 
     var friendItemList = arrayListOf<FriendItem>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var fragment: FriendListFragment = FriendListFragment()
+        supportFragmentManager.beginTransaction().add(R.id.frMainFriendList, fragment).commit()
     }
+
+
 }
